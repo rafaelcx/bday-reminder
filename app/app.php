ca3c5345177197f469fd605fe6db6e7e62e62153
Rafael Garcia <rafaelgarcia95@gmail.com>
2025-04-27 23:26:37 -0300

    Add twig-view dependency

--- app/app.php
+++ app/app.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -11,5 +13,9 @@ $app = AppFactory::create();
 // Registering routes
 $routes = require __DIR__ . '/../app/routes.php';
 $routes($app);
+
+// Add Twig-View middleware
+$twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+$app->add(TwigMiddleware::create($app, $twig));
 
 return $app;
