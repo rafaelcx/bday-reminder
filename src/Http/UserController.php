6e22421219d25aa3b0000519ebf08cafd65c596f
Rafael Garcia de Carvalho <rafaelgarcia95@gmail.com>
2025-06-08 02:26:07 -0300

    Fetch birthdays from repository (#13)

--- src/Http/UserController.php
+++ src/Http/UserController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http;
 
+use App\Repository\Birthday\BirthdayRepositoryResolver;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
@@ -11,23 +12,14 @@ use Slim\Views\Twig;
 class UserController {
 
     public function show(Request $request, Response $response): Response {
+        $user_uid = $request->getQueryParams()['uid'];
+
+        $birthday_repository = BirthdayRepositoryResolver::resolve();
+
         $view = Twig::fromRequest($request);
         return $view->render($response, 'user.html.twig', [
-            'birthdays' => $this->generateRandomBirthday(),
+            'birthdays' => $birthday_repository->findByUserUid($user_uid),
         ]);
-    }
-
-    private function generateRandomBirthday(): array {
-        $b_array = [];
-        for ($i = 0; $i <= 20; $i++) {
-            $b = new \stdClass;
-            $b->id = rand(1, 10000);
-            $b->name = 'Rafael Garcia de Carvalho e Outro Sobrenome e Outro';
-            $b->date = '1995-11-30';
-
-            $b_array[] = $b;
-        }
-        return $b_array;
     }
 
 }
--- test/src/Http/UserControllerTest.php
+++ test/src/Http/UserControllerTest.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace Test\Src\Http;
 
+use App\Repository\Birthday\BirthdayRepositoryResolver;
 use Test\CustomTestCase;
 
 class UserControllerTest extends CustomTestCase {
 
     public function testController_WhenSuccessful(): void {
+        $birthday_repository = BirthdayRepositoryResolver::resolve();
+        $birthday_repository->create('1', 'Name One', new \DateTime());
+        $birthday_repository->create('1', 'Name Two', new \DateTime());
+        $birthday_repository->create('2', 'Name Three', new \DateTime());
+       
         $result = $this->request_simulator
             ->withMethod('GET')
             ->withBody([])
@@ -20,7 +26,24 @@ class UserControllerTest extends CustomTestCase {
 
         $result_body = (string) $result->getBody();
         $this->assertStringContainsString('Your Birthday List', $result_body);
-        $this->assertStringContainsString('Rafael Garcia de Carvalho e Outro Sobrenome e Outro', $result_body);
+        $this->assertStringContainsString('Name One', $result_body);
+        $this->assertStringContainsString('Name Two', $result_body);
+        $this->assertStringNotContainsString('Name Three', $result_body);
+    }
+
+    public function testController_WhenSuccessful_AndUserHasNoBirthdays(): void {
+        $result = $this->request_simulator
+            ->withMethod('GET')
+            ->withBody([])
+            ->withPath('/user')
+            ->withQueryParam('uid', '1')
+            ->dispatch();
+
+        $this->assertSame(200, $result->getStatusCode());
+
+        $result_body = (string) $result->getBody();
+        $this->assertStringContainsString('Your Birthday List', $result_body);
+        $this->assertStringContainsString('You haven\'t added any birthdays yet', $result_body);
     }
 
 }
