735b73d309f157b9b4db5bc33f5e53aeda40ac14
Rafael Garcia de Carvalho <rafaelgarcia95@gmail.com>
2025-08-20 15:15:35 -0300

    Use Clock utility on credential and cfg entities (#46)

--- src/Repository/UserConfig/UserConfig.php
+++ src/Repository/UserConfig/UserConfig.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace App\Repository\UserConfig;
 
-class UserConfig {
+use App\Utils\Clock;
 
-    // TODO: Change DateTime types to clock
+class UserConfig {
 
     public function __construct(
         public readonly string $uid,
         public readonly string $user_uid,
         public readonly string $name,
         public readonly string $value,
-        public readonly \DateTime $created_at,
-        public readonly \DateTime $updated_at,
+        public readonly Clock $created_at,
+        public readonly Clock $updated_at,
     ) {}
 
 }
