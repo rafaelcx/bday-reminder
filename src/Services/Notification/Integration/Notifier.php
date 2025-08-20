8b721588a70f22ede4d05605fae24fe882f0bd07
Rafael Garcia de Carvalho <rafaelgarcia95@gmail.com>
2025-08-20 17:57:55 -0300

    Improve Notifier interface (#50)

--- src/Services/Notification/Integration/Notifier.php
+++ src/Services/Notification/Integration/Notifier.php
@@ -10,12 +10,6 @@ use App\Repository\User\User;
 interface Notifier {
 
     public function notify(User $user, Birthday ...$birthday_list): void;
-
-    // TODO: Delete messages should not be exposed as an interface method. It should expose only a getUpdates
-    // method since message deletion is specific to this integration, as I want to clear the telegram chat
-    // after successfully retrieving updates. Notifier.php has nothing to do with it tough
-    
     public function getUpdates(): array;
-    public function deleteMessages(array $updates): void;
 
 }
