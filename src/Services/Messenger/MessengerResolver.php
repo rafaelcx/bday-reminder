4fa2e73483949788af5e6792c8396bd7ba005491
Rafael Garcia de Carvalho <rafaelgarcia95@gmail.com>
2026-05-17 23:26:12 -0300

    Create generic Messenger service (#79)

---
+++ src/Services/Messenger/MessengerResolver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services\Messenger;
+
+use App\Services\Messenger\Telegram\TelegramMessenger;
+use App\Utils\StaticScope;
+
+class MessengerResolver {
+
+    public static function resolve(): Messenger {
+        return StaticScope::getOrCreate(self::class, 'instance', self::createInstance(...));
+    }
+
+    private static function createInstance(): Messenger {
+        return new TelegramMessenger();
+    }
+
+}
